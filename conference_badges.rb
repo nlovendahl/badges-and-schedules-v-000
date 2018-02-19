1a815133bdec85e652f08783bfce99989ba2b3d3
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-19 19:01:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,7 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   "Hello, my name is #{name}."
+end
 
 def batch_badge_creator(speakers)
   badges = []
