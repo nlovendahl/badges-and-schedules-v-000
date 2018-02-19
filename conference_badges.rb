d7a1552563b850e791ee36c10179f180d3743621
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-19 18:57:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(speakers)
   badges = []
   speakers.each do |name|
-    badges << "Hello, my name is #{name}"
+    badges << "Hello, my name is #{name}."
   end
   badges
 end
